efe5495835b9fb2cbabae774b1228fc0bdf94e10
parth181-oss <parthsarthi181@gmail.com>
2026-07-06 16:56:04 +0530

    lec6
    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// if score =33 => Number(33)
+// score =33abc => Number(NaN)
+//score = null => Number(0)
+//score = undefined => number(Nan)
+// true = 1; false =0
+
+// boolean 
+// 1=>true, 0=> false , ""=> false "string" => true
+
+
+
