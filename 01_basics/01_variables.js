334a54b207b2af41acf8b23496a0f48b7c92a73a
parth181-oss <parthsarthi181@gmail.com>
2026-07-06 16:15:33 +0530

    lec4
    let , const , var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "parth@gmail.com"
+var accountPassword = "98765"
+accountCity = "Dhanbad"
+let accountState
+
+// accountId = 4347 not allowed
+/*
+prefer not to use var 
+semicolon lgao nhi lgao koi farq nhi hai
+*/
+
+accountEmail = "ps@gmail.com"
+accountPassword = "23456"
+accountCity = "Ranchi"
+console.log(accountId)
+console.table([accountId , accountEmail, accountPassword , accountCity,accountState])
