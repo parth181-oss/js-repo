3ee03ae37d5c9fcc73bade3b247ace1a04bb7dd7
parth181-oss <parthsarthi181@gmail.com>
2026-07-06 16:44:15 +0530

    lec5
    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+alert()
+
+// code redability should be high
+
+let name = "Parth" //string
+let age  = 18 //number
+let isLoggedIn = false //bool
+
+
+
+/*DataTypes
+
+number => 2^53
+bigint
+string =>""
+bollean => true/false
+null => standalone value
+undefined
+symbol => uniqueness
+object
+
+typeof => null => object
+*/
