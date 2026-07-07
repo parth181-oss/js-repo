0441da0bfbbe029000445361ec3293ec3d257ae3
parth181-oss <parthsarthi181@gmail.com>
2026-07-07 13:57:48 +0530

    lec 7
    Operaations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,33 @@
+/*  Operations     */
+
+let value =3
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**3); //power => 2 ka power 3
+
+let s1 = "hello"
+let s2 = " parth"
+let s3 = s1+s2
+console.log(s3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true); //=> true
+console.log(+true); // =>1
+console.log(""); // => 
+console.log(+""); // => 0
+
+let gameCounter = 100
+++gameCounter // or gameCounter++ => same as cpp
+console.log(gameCounter);
+
+
+
