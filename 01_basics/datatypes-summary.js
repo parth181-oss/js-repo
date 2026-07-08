21141d1c7357405a57686332f83080f0d63880f9
parth181-oss <parthsarthi181@gmail.com>
2026-07-08 18:27:32 +0530

    lec09
    summary of datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+/*  
+
+      Primitive ( call by value)
+
+    7 Types : string , number, boolean, null, undefined , symbol, BigInt(number ke last me n lga do)
+
+
+
+    Reference (non Primitive)
+
+    Array, Objects, Functions
+
+
+*/
+
+// Array , Objects ,  function
+
+const cricketers = ["sachin","dhoni","kohli"]; //Array
+
+let myDetail = {
+    name: "Parth",
+    age: 19,
+}  // Object
+
+const myFunc =function(){
+    console.log("hello world")
+} // function
+
+
